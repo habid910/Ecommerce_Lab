e09a374a89db7fe4fba7623ebbe531fd95396fc8
Abid Hussain <H.abid910@gmail.com>
2020-01-30 19:27:17 -0500

    first commit

---
+++ Customers/Controllers/HomeController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Customers.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        Models.CustomersEntities1 db = new Models.CustomersEntities1();
+        public ActionResult Index()
+        {
+            return View(db.Customers);
+        }
+
+        // GET: Home/Details/5
+        public ActionResult Details(int id)
+        {
+            Models.Customer theCustomer = db.Customers.SingleOrDefault(c => c.customer_id == id);
+            return View(theCustomer);
+        }
+
+        // GET: Home/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Home/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                Models.Customer customer = new Models.Customer();
+                customer.name = collection["name"];
+                customer.number = collection["number"];
+                customer.address = collection["address"];
+                db.Customers.Add(customer);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Home/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Models.Customer theCustomer = db.Customers.SingleOrDefault(c => c.customer_id == id);
+            return View(theCustomer);
+        }
+
+        // POST: Home/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                Models.Customer theCustomer = db.Customers.SingleOrDefault(c => c.customer_id == id);
+                theCustomer.name = collection["name"];
+                theCustomer.number = collection["number"];
+                theCustomer.address = collection["address"];
+
+               db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Home/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Models.Customer theCustomer = db.Customers.SingleOrDefault(c => c.customer_id == id);
+            return View(theCustomer);
+        }
+
+        // POST: Home/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                Models.Customer theCustomer = db.Customers.SingleOrDefault(c => c.customer_id == id);
+                theCustomer.name = collection["name"];
+                theCustomer.number = collection["number"];
+                theCustomer.address = collection["number"];
+                db.Customers.Remove(theCustomer);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Customers/Controllers/OrderController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Customers.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Order
+        Models.CustomersEntities1 db = new Models.CustomersEntities1();
+        public ActionResult Index(int id)
+        {
+            ViewBag.id = id;
+            return View(db.Orders.Where(c=> c.customer_id==id));
+        }
+
+        // GET: Order/Details/5
+        public ActionResult Details(int id)
+        {
+            Models.Order theOrder = db.Orders.SingleOrDefault(o => o.order_id == id);
+            return View(theOrder);
+        }
+
+        // GET: Order/Create
+        public ActionResult Create(int id)
+        {
+            ViewBag.id = id;
+            return View();
+        }
+
+        // POST: Order/Create
+        [HttpPost]
+        public ActionResult Create(int id,FormCollection collection)
+        {
+            
+            try
+            {
+                // TODO: Add insert logic here
+                Models.Order order = new Models.Order()
+                {
+                    customer_id = id,
+                   order_item = collection["order_item"],
+                    status = collection["status"]
+                 
+                };
+                
+                db.Orders.Add(order);
+                db.SaveChanges();
+
+                return RedirectToAction("Index",new {id=id });
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Order/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Models.Order theOrder = db.Orders.SingleOrDefault(c => c.order_id == id);
+            return View(theOrder);
+        }
+
+        // POST: Order/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                Models.Order theOrder = db.Orders.SingleOrDefault(o => o.order_id == id);
+                theOrder.order_item = collection["order_item"];
+                theOrder.status = collection["status"];
+                
+                db.SaveChanges();
+
+                return RedirectToAction("Index",new {id=theOrder.customer_id });
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Order/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Models.Order theOrder = db.Orders.SingleOrDefault(o=> o.order_id ==id);
+            return View(theOrder);
+        }
+
+        // POST: Order/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                Models.Order theOrder = db.Orders.SingleOrDefault(o=> o.order_id==id);
+                theOrder.order_item = collection["order_item"];
+                theOrder.status = collection["status"];
+                db.Orders.Remove(theOrder);
+                db.SaveChanges();
+
+                return RedirectToAction("Index",new {id=theOrder.customer_id });
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
